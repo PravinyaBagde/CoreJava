fc0df471ce2ce8f78b010dc3981fb283feab783e
PravinyaBagde <bagdepravinya@gmail.com>
2025-12-22 15:53:56 +0530

    Ascending order Sorted Array

---
+++ 01_Basics/AscendingOrder.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class AscendingOrder {
+    public static void main(String[] args) {
+        int[] arr = {12, 34, 89, 92, 45, 18, 7};
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        System.out.println("Ascending Order Array is : " + Arrays.toString(arr));
+    }
+}
